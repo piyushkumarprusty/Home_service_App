d8abf752bd48b739d7a9d279b427f0ae4e20eed4
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-01 00:48:46 +0530

    order activity don

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.android.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.android.myapplication.Adapters.MainAdapter;
+import com.example.android.myapplication.Models.MainModel;
+import com.example.android.myapplication.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayList<MainModel> list = new ArrayList<>();
+        MainModel m = new MainModel(R.drawable.car2 ,"tt","12" , "do" );
+        list.add(m);
+
+        list.add(new MainModel(R.drawable.car , "screw" , "10" , "This is to repair budget"));
+        list.add(new MainModel(R.drawable.car2 , "screw" , "10" , "This is to repair budget"));
+        list.add(new MainModel(R.drawable.as , "washing" , "40" , "This is to repair washing macihine"));
+        list.add(new MainModel(R.drawable.car2 , "screw" , "10" , "This is to repair budget"));
+        list.add(new MainModel(R.drawable.as , "fridge" , "50" , "This is to repair fridge"));
+
+
+        MainAdapter adapter = new MainAdapter(list ,this);
+        binding.recyclerview.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recyclerview.setLayoutManager(layoutManager);
+
+
+
+
+
     }
 }
