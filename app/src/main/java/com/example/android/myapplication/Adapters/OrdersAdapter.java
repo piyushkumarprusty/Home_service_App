7e82aba19dfcb5279d10aa2325a9f9c4e116ae4a
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-03 01:16:31 +0530

    OrdersAdapter.java extends recycler.adapter implemt

--- app/src/main/java/com/example/android/myapplication/Adapters/OrdersAdapter.java
+++ app/src/main/java/com/example/android/myapplication/Adapters/OrdersAdapter.java
@@ -1,23 +1,46 @@
 package com.example.android.myapplication.Adapters;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android.myapplication.Models.OrdersModel;
 import com.example.android.myapplication.R;
 
-public class OrdersAdapter {
+import java.util.ArrayList;
 
+public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.viewHolder> {
+    ArrayList<OrdersModel> list;
+    Context context;
 
-    public class viewHolder extends RecyclerView.ViewHolder{
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrdersAdapter.viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
 
         ImageView orderImage;
-        TextView soldItemName, orderNumber , price;
+        TextView soldItemName, orderNumber, price;
 
-        public viewHolder(@NonNull  View itemView) {
+        public viewHolder(@NonNull View itemView) {
             super(itemView);
             orderImage = itemView.findViewById(R.id.orderImage);
             soldItemName = itemView.findViewById(R.id.orderItemName);
