b656f48fb8d3df182d7c4ec78d6bc1d357f6358f
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-03 01:06:04 +0530

    OrdersAdapter.java extends recy

--- app/src/main/java/com/example/android/myapplication/Adapters/OrdersAdapter.java
+++ app/src/main/java/com/example/android/myapplication/Adapters/OrdersAdapter.java
@@ -1,4 +1,20 @@
 package com.example.android.myapplication.Adapters;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class OrdersAdapter {
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        ImageView orderImage;
+        TextView soldImage , orderNumber , price;
+
+        public viewHolder(@NonNull  View itemView) {
+            super(itemView);
+        }
+    }
 }
