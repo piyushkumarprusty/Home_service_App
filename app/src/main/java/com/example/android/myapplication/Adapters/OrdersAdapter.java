55c9dd060dcc713dbc100ef1e3f644331ae47504
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-03 01:25:37 +0530

    OrdersAdapter.java don

--- app/src/main/java/com/example/android/myapplication/Adapters/OrdersAdapter.java
+++ app/src/main/java/com/example/android/myapplication/Adapters/OrdersAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.myapplication.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -18,21 +19,31 @@ public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.viewHolder
     ArrayList<OrdersModel> list;
     Context context;
 
+    public OrdersAdapter(ArrayList<OrdersModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.order_sample, parent, false);
+        return new viewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull OrdersAdapter.viewHolder holder, int position) {
 
+        final OrdersModel model = list.get(position);
+        holder.orderImage.setImageResource(model.getOrderImage());
+        holder.soldItemName.setText(model.getSoldItemName());
+        holder.orderNumber.setText(model.getOrderNumber());
+        holder.price.setText(model.getPrice());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class viewHolder extends RecyclerView.ViewHolder {
