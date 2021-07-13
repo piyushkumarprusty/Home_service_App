13a1a41ae6b58b35ac660437582f58f676135aeb
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-13 23:44:40 +0530

    OrderActivity.java add list

--- app/src/main/java/com/example/android/myapplication/OrderActivity.java
+++ app/src/main/java/com/example/android/myapplication/OrderActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.android.myapplication.Models.OrdersModel;
 import com.example.android.myapplication.databinding.ActivityOrderBinding;
+
+import java.util.ArrayList;
 
 public class OrderActivity extends AppCompatActivity {
 ActivityOrderBinding binding;
@@ -13,5 +16,19 @@ ActivityOrderBinding binding;
         super.onCreate(savedInstanceState);
         binding = ActivityOrderBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ArrayList<OrdersModel> list =  new ArrayList<>();
+        OrdersModel ordersModel = new OrdersModel(R.drawable.as,"Hosuer re" , "28" , "REw123");
+        list.add(ordersModel);
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
+        list.add(new OrdersModel(R.drawable.car2,"car repair" , "20" ,"ASDF123" ));
     }
 }
