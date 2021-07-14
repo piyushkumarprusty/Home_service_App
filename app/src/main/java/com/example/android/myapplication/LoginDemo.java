9bdac997174f290e15f18cc387c4172b871cb119
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-14 16:08:24 +0530

    LoginDemo.java user input

--- app/src/main/java/com/example/android/myapplication/LoginDemo.java
+++ app/src/main/java/com/example/android/myapplication/LoginDemo.java
@@ -3,8 +3,20 @@ package com.example.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginDemo extends AppCompatActivity {
+
+    public void login(View view){
+
+        Log.i("Info" , "Button Pressed");
+        EditText userEditText = findViewById(R.id.editTextTextPassword);
+        EditText username = findViewById(R.id.userName);
+        Log.i("USernamse" , username.getText().toString());
+        Log.i("Passowrd" , userEditText.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
